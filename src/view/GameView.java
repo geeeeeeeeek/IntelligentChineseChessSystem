9916c5a9461589d6fa0b9f651a1c406868a3b859
geeeeeeeeek <810421910@qq.com>
2014-12-11 00:56:42 +0800

    greatly optimized loading speed by compressing images, and fulfilled basic logic of moving pieces

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -4,7 +4,6 @@ import chess.Board;
 import chess.Piece;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.HashMap;
@@ -14,33 +13,33 @@ import java.util.Map;
  * Created by Tong on 12.10.
  */
 public class GameView {
-    Map<String, JLabel> pieceObjects = new HashMap<String, JLabel>();
+    private Map<String, JLabel> pieceObjects = new HashMap<String, JLabel>();
     private static final int VIEW_WIDTH = 700, VIEW_HEIGHT = 712;
     private static final int PIECE_WIDTH = 67, PIECE_HEIGHT = 67;
     private static final int SY_COE = 68, SX_COE = 68;
     private static final int SX_OFFSET = 50, SY_OFFSET = 15;
+    private static String selectedPieceKey = null;
 
     public void init(Board board) {
-        final JFrame frame = new JFrame("Intelligent Chinese Chess - dev by zhongyi.tong");
-        frame.setIconImage(new ImageIcon("res/img/rb.png").getImage());
+        final JFrame frame = new JFrame("Intelligent Chinese Chess - Dev by Zhongyi.TONG");
+        frame.setIconImage(new ImageIcon("res/img/icon.png").getImage());
         frame.setSize(VIEW_WIDTH, VIEW_HEIGHT + 40);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JLayeredPane pane = new JLayeredPane();
         frame.add(pane);
 
-        ImageIcon boardIcon = new ImageIcon("res/img/board.png");
-        boardIcon.setImage(boardIcon.getImage().getScaledInstance(VIEW_WIDTH, VIEW_HEIGHT, Image.SCALE_DEFAULT));
-        JLabel bgBoard = new JLabel(boardIcon);
+        JLabel bgBoard = new JLabel(new ImageIcon("res/img/board.png"));
         bgBoard.setLocation(0, 0);
         bgBoard.setSize(VIEW_WIDTH, VIEW_HEIGHT);
         bgBoard.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                int[] sPos = new int[]{e.getXOnScreen() - frame.getX(), e.getYOnScreen() - frame.getY()};
-                int[] pos = viewToModelConverter(sPos);
-                JOptionPane.showMessageDialog(null, pos[0] + " " + pos[1], "alert", JOptionPane.ERROR_MESSAGE);
-                super.mouseClicked(e);
+                if (selectedPieceKey != null) {
+                    int[] sPos = new int[]{e.getXOnScreen() - frame.getX(), e.getYOnScreen() - frame.getY()};
+                    int[] pos = viewToModelConverter(sPos);
+                    movePieceInView(selectedPieceKey, pos);
+                }
             }
         });
         pane.add(bgBoard, 1);
@@ -49,26 +48,24 @@ public class GameView {
         for (Map.Entry<String, Piece> stringPieceEntry : pieces.entrySet()) {
             String key = stringPieceEntry.getKey();
             int[] pos = stringPieceEntry.getValue().position;
-            ImageIcon pieceIcon = new ImageIcon("res/img/" + key.substring(0, 2) + ".png");
-            pieceIcon.setImage(pieceIcon.getImage().getScaledInstance(PIECE_WIDTH, PIECE_HEIGHT, Image.SCALE_DEFAULT));
-            JLabel bgpiece = new JLabel(pieceIcon);
+            JLabel bgpiece = new JLabel(new ImageIcon("res/img/" + key.substring(0, 2) + ".png"));
 
             int[] sPos = modelToViewConverter(pos);
             bgpiece.setLocation(sPos[0], sPos[1]);
             bgpiece.setSize(PIECE_WIDTH, PIECE_HEIGHT);
-            bgpiece.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    int[] sPos = new int[]{e.getXOnScreen() - frame.getX(), e.getYOnScreen() - frame.getY()};
-                    int[] pos = viewToModelConverter(sPos);
-                    JOptionPane.showMessageDialog(null, pos[0] + " " + pos[1], "alert", JOptionPane.ERROR_MESSAGE);
-                    super.mouseClicked(e);
-                }
-            });
+            bgpiece.addMouseListener(new PieceOnClickListener(key));
             pieceObjects.put(stringPieceEntry.getKey(), bgpiece);
             pane.add(bgpiece, 0);
         }
         frame.setVisible(true);
+    }
+
+    public void movePieceInView(String pieceKey, int[] to) {
+        JLabel pieceObject = pieceObjects.get(pieceKey);
+        int[] sPos = modelToViewConverter(to);
+        pieceObject.setLocation(sPos[0], sPos[1]);
+        /* Clear 'from' and 'to' info on the board */
+        selectedPieceKey = null;
     }
 
     private int[] modelToViewConverter(int pos[]) {
@@ -83,4 +80,18 @@ public class GameView {
         int y = (sPos[0] - SX_OFFSET) / SX_COE, x = (sPos[1] - SY_OFFSET - ADDITIONAL_SY_OFFSET) / SY_COE;
         return new int[]{x, y};
     }
+
+    public class PieceOnClickListener extends MouseAdapter {
+        private String key;
+
+        public PieceOnClickListener(String key) {
+            this.key = key;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            GameView.selectedPieceKey = key;
+        }
+    }
+
 }
